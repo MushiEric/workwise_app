64636056e7f569119b30691cc7433f3807677c86
MushiEric <ericprosper603@gmail.com>
2026-02-24 10:43:57 +0300

    feat: update Google Maps API key to use AppConstant.googlemapKey for geofence map

--- projects/flutter/workwise_erp/lib/core/constants/app_constant.dart
+++ projects/flutter/workwise_erp/lib/core/constants/app_constant.dart
@@ -9,8 +9,8 @@ static const String website = 'https://getcore.workwise.africa';
 static const String supportEmail = 'support@getcore.workwise.africa';
 static const String privacyPolicyUrl = 'https://getcore.workwise.africa/privacy-policy';
 static const String termsOfServiceUrl = 'https://getcore.workwise.africa/terms ';
-static const String googleMapsApiKey = 'AIzaSyC1SKvVzNxrjWMCO5pqa89AIjlEE2aFIxY';
-
+// static const String googleMapsApiKey = 'AIzaSyC1SKvVzNxrjWMCO5pqa89AIjlEE2aFIxY';
+static const String googlemapKey = "AIzaSy  ARuZs3KqeodlNXnSeJUWcHW_zVQ3ZFc0w";
 
 
  }
